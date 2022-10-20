8bc01e1b94d779a8d113974ddd4b01bf832adcab
Ebaerdenebat12345 <is21d010@mandakh.edu.mn>
2022-10-20 08:39:06 -0700

    safaf

--- social/src/component/Topbar/Topbar.jsx
+++ social/src/component/Topbar/Topbar.jsx
@@ -1,11 +1,23 @@
 import "./topbar.css"
+import { Search} from "@mui/icons-material"
 
 export default function Topbar() {
   return (
     <div className="topbarContainer">
-      <div className="topbarLeft"></div>
-      <div className="topbarCenter"></div>
-      <div className="topbarRight"></div>
+      <div className="topbarLeft"> 
+      <span className="logo">Lamasocial</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+        <Search/>
+        <input placeholder="search for friend, post or video " className="searchinput" />
+
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks"></div>
+
+      </div>
       
     </div>
   )
