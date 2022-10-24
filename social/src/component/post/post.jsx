3c42604d22e015c339fc918e38bcc19b23a25089
Ebaerdenebat12345 <is21d010@mandakh.edu.mn>
2022-10-24 08:31:33 -0700

    ahahha

--- social/src/component/post/post.jsx
+++ social/src/component/post/post.jsx
@@ -1,9 +1,32 @@
 import "./post.css"
+import {MoreVert} from "@mui/icons-material"
 
 export default function Post() {
   return (
-    <div>
-        hello mf
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img 
+            className="postProfileImg"  
+            src="/ebabaa/person/teamm.png" 
+            alt="" 
+            />
+            <span className="postUsername">safak kacaoglu</span>
+            <span className="postDate">5 mins ago</span>
+
+          </div>
+          <div className="postTopRight">
+            <MoreVert/>
+          </div>
+
+        </div>
+        <div className="postCenter"></div>
+        <div className="postBottom"></div>
+
+        
+         </div>
+        
     </div>
   )
 }
